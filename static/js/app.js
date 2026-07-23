2546653e7dae9733363085f5c04e05daececc22b
Mohit Surewal <187611416+MohitSurewal@users.noreply.github.com>
2026-07-23 20:26:08 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,11 @@
 console.log("Surewal.tech Loaded");
+
+const menu=document.querySelector(".hamburger");
+
+const nav=document.querySelector(".nav-links");
+
+menu.addEventListener("click",()=>{
+
+    nav.classList.toggle("active");
+
+});
